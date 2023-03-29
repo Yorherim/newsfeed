2b5d91183d8492a51fa6aa20f1e01594a16a37c0
Igor Trifonov <yorherim@gmail.com>
2023-03-29 08:42:50 +0800

    add data at page with template

---
+++ script.js
@@ -0,0 +1,307 @@
+const data = {
+  sources: [
+    { id: 1, name: "meduza" },
+    { id: 6, name: "3dnews" },
+    { id: 7, name: "nytimes" },
+    { id: 8, name: "forbes" },
+    { id: 9, name: "igromania" },
+    { id: 10, name: "buro237" },
+    { id: 11, name: "rusvesna" },
+    { id: 12, name: "7ya" },
+    { id: 14, name: "aif" },
+    { id: 15, name: "gazetaru" },
+    { id: 16, name: "karpov.courses" },
+  ],
+  categories: [
+    { id: 1, name: "tech" },
+    { id: 2, name: "sport" },
+    { id: 3, name: "fashion" },
+    { id: 4, name: "politics" },
+    { id: 5, name: "other" },
+    { id: 6, name: "karpov.courses" },
+  ],
+  items: [
+    {
+      id: 328672,
+      lang: "ru",
+      date: "Wed Mar 29 2023 01:36:23 GMT+0300 (Moscow Standard Time)",
+      title: "В Киеве демонтировали памятник советскому истребителю МиГ-19",
+      description: "Он был установлен в 1986 году на территории школы.",
+      image:
+        "https://aif-s3.aif.ru/images/031/758/744a0b53cce7e0c63ca483aec592d35a.jpg",
+      source_id: 14,
+      category_id: 4,
+    },
+    {
+      id: 328694,
+      lang: "ru",
+      date: "Wed Mar 29 2023 01:29:54 GMT+0300 (Moscow Standard Time)",
+      title:
+        "Кабмин РФ: Россия по просьбе Будапешта осуществит дополнительные поставки газа в Венгрию",
+      description:
+        'Власти России заявили, что Москва продолжит поставлять газ в Венгрию согласно договоренностям, а также осуществит дополнительные поставки по просьбе Будапешта. Об этом сообщается на сайте российского правительства.\n\n"Россия продолжает ...',
+      image:
+        "https://img.gazeta.ru/files3/377/14048377/2021-10-01T064852Z_1655088607_RC2U0Q9MMEDQ_RTRMADP_3_HUNGARY-RUSSIA-GAS-UKRAINE-pic905-895x505-31955.jpg",
+      source_id: 15,
+      category_id: 4,
+    },
+    {
+      id: 328697,
+      lang: "ru",
+      date: "Wed Mar 29 2023 01:18:32 GMT+0300 (Moscow Standard Time)",
+      title:
+        "Американский журналист Закария предсказал крушение экономики США из-за ослабления доллара",
+      description:
+        'США столкнутся с невиданных масштабов экономическим кризисом, когда доллар прекратит доминировать на глобальном рынке. Таким мнением поделился обозреватель Фарид Закария в эфире телеканала CNN . \n\n"Украинский кризис в сочетании со все ...',
+      image:
+        "https://img.gazeta.ru/files3/33/16456033/2023-03-10T155657Z_1695635831_RC22RZ9OFY5J_RTRMADP_3_SVB-FINANCIAL-GROUP-STOCK-pic905-895x505-81133.jpg",
+      source_id: 15,
+      category_id: 4,
+    },
+    {
+      id: 328678,
+      lang: "ru",
+      date: "Wed Mar 29 2023 01:17:59 GMT+0300 (Moscow Standard Time)",
+      title:
+        "Киев уведомил УПЦ о разрыве договора аренды Киево-Печерской лавры",
+      description:
+        "Договор от 2013 года будет считаться разорванным с 29 марта.",
+      image:
+        "https://aif-s3.aif.ru/images/031/758/ef6ae89e23b7dbdaed0a3913638d3062.jpg",
+      source_id: 14,
+      category_id: 5,
+    },
+    {
+      id: 328666,
+      lang: "ru",
+      date: "Wed Mar 29 2023 01:15:57 GMT+0300 (Moscow Standard Time)",
+      title:
+        "Минфин раскрыл параметры софинансирования сбережений россиян на старость",
+      description:
+        "Минфин раскрыл принципы софинансирования из бюджета добровольных накоплений россиян на старость. Программа будет финансироваться за счет средств Фонда национального благосостояния и Социального фонда. Россияне смогут получать от государства до 36 000 рублей в год стимулирующих выплат",
+      image: "",
+      source_id: 8,
+      category_id: 5,
+    },
+    {
+      id: 328682,
+      lang: "ru",
+      date: "Wed Mar 29 2023 01:08:03 GMT+0300 (Moscow Standard Time)",
+      title:
+        "Принц Гарри обвинил королевскую семью в утаивании прослушки его телефона",
+      description:
+        "По его словам, королевская семья Британии скрывала эту информацию, чтобы самой не оказаться втянутой в скандал.",
+      image:
+        "https://aif-s3.aif.ru/images/031/758/c2d7f14a7a53167ef436841cd5a49e13.jpg",
+      source_id: 14,
+      category_id: 4,
+    },
+    {
+      id: 328695,
+      lang: "ru",
+      date: "Wed Mar 29 2023 00:57:00 GMT+0300 (Moscow Standard Time)",
+      title:
+        "Microsoft анонсировала интерактивный дисплей Surface Hub 2S второго поколения с необычной Windows",
+      description:
+        "Компания Microsoft без лишнего шума представила интерактивный дисплей Surface Hub 2S второго поколения, ориентированный на корпоративных пользователей. О новинке производитель рассказал в своём блоге. К сожалению, технические характеристики устройства компания не указала.",
+      image:
+        "https://3dnews.ru/assets/external/illustrations/2023/03/29/1084171/microsoft1.jpg",
+      source_id: 6,
+      category_id: 1,
+    },
+    {
+      id: 328683,
+      lang: "ru",
+      date: "Wed Mar 29 2023 00:52:37 GMT+0300 (Moscow Standard Time)",
+      title:
+        "Кадыров сообщил о ликвидации двух боевиков в частном секторе Гудермеса",
+      description:
+        "Боевикам предложили сдаться, но они бросили в сторону правоохранителей гранату.",
+      image:
+        "https://aif-s3.aif.ru/images/031/758/5445bd1d71471dcd8a1d82b9abe4caa9.png",
+      source_id: 14,
+      category_id: 5,
+    },
+    {
+      id: 328708,
+      lang: "ru",
+      date: "Wed Mar 29 2023 00:40:00 GMT+0300 (Moscow Standard Time)",
+      title:
+        "На мобильные игры и приложения человечество потратило $33,9 млрд с января — 64 % пришлось на iOS",
+      description:
+        "За первый квартал 2023 года пользователи операционных систем iOS и Android потратили $33,9 млрд на мобильные приложения и игры, говорит свежий анализ рынка от аналитиков из Data.ai.",
+      image:
+        "https://3dnews.ru/assets/external/illustrations/2023/03/29/1084170/qwdqwdqdqwd.jpg",
+      source_id: 6,
+      category_id: 1,
+    },
+    {
+      id: 328684,
+      lang: "ru",
+      date: "Wed Mar 29 2023 00:35:37 GMT+0300 (Moscow Standard Time)",
+      title:
+        "В Сеть попало видео, на котором Байдену дают инструкции перед выступлением",
+      description:
+        "На кадрах запечатлено, как один из сопровождающих ведёт президента США к импровизированной трибуне.",
+      image:
+        "https://aif-s3.aif.ru/images/031/758/2faab84eced70b2f2ce6bb92599d780a.jpg",
+      source_id: 14,
+      category_id: 5,
+    },
+    {
+      id: 328699,
+      lang: "ru",
+      date: "Wed Mar 29 2023 00:28:36 GMT+0300 (Moscow Standard Time)",
+      title:
+        "Канада направит Украине через МВФ дополнительный кредит на $1,76 млрд",
+      description:
+        "Власти Канады выделят Украине посредством Международного валютного фонда (МВФ) дополнительный кредит на $1,76 млрд (2,4 млрд канадских долларов) в 2023 году. Об этом сообщает ТАСС со ссылкой на проект федерального бюджета на грядущий ...",
+      image:
+        "https://img.gazeta.ru/files3/154/13525154/20160704_zap_h140_010-pic905-895x505-31327.jpg",
+      source_id: 15,
+      category_id: 4,
+    },
+    {
+      id: 328685,
+      lang: "ru",
+      date: "Wed Mar 29 2023 00:22:56 GMT+0300 (Moscow Standard Time)",
+      title:
+        "США ввели экспортные ограничения в отношении двух российских фирм",
+      description: "Речь идет о фирмах «Авиатех» и «Авиазапчасть».",
+      image:
+        "https://aif-s3.aif.ru/images/031/758/f755162173d5796a3dad16fa56cf6fc3.jpg",
+      source_id: 14,
+      category_id: 5,
+    },
+    {
+      id: 328686,
+      lang: "ru",
+      date: "Wed Mar 29 2023 00:10:02 GMT+0300 (Moscow Standard Time)",
+      title: "На Грузию обрушился мощный ураган",
+      description:
+        "Погиб один человек, ещё трое, в том числе несовершеннолетний, получили травмы.",
+      image:
+        "https://aif-s3.aif.ru/images/031/758/f47d6fd8fc9498ac3e47d4ba9d68326b.jpg",
+      source_id: 14,
+      category_id: 5,
+    },
+    {
+      id: 328701,
+      lang: "ru",
+      date: "Wed Mar 29 2023 00:03:44 GMT+0300 (Moscow Standard Time)",
+      title: "Сербия намерена закупать газ в Греции через Болгарию",
+      description:
+        "Сербия планирует закупать до 3,5 млн кубометров газа ежедневно в терминале для сжиженного природного газа в греческом Александруполисе и поставлять их через строящийся интерконнектор с Болгарией. Об этом во вторник сообщил сербский лидер ...",
+      image: "",
+      source_id: 15,
+      category_id: 4,
+    },
+    {
+      id: 328687,
+      lang: "ru",
+      date: "Wed Mar 29 2023 00:00:09 GMT+0300 (Moscow Standard Time)",
+      title:
+        "РФ продолжает дополнительные поставки газа в Венгрию по просьбе Будапешта",
+      description:
+        "В правительстве РФ отметили, что Венгрия остается единственной страной ЕС, которая отстаивает свои нацинтересы.",
+      image:
+        "https://aif-s3.aif.ru/images/031/758/2dd1204702520d352d8f4a0c4ed36184.jpg",
+      source_id: 14,
+      category_id: 5,
+    },
+    {
+      id: 328720,
+      lang: "ru",
+      date: "Wed Mar 29 2023 00:00:00 GMT+0300 (Moscow Standard Time)",
+      title:
+        "Компьютер месяца, спецвыпуск. Собираем максимально дешевые игровые ПК, используя разные торговые площадки",
+      description:
+        "В рамках этого материала мы рассмотрели покупку комплектующих для сборки игровых системных блоков на разных торговых площадках: в крупных сетевых магазинах России, на AliExpress и «Авито». О том, где выгоднее покупать компьютерное железо, читайте далее",
+      image:
+        "https://3dnews.ru/assets/external/illustrations/2023/03/25/1084029/sm.0.800.jpg",
+      source_id: 6,
+      category_id: 1,
+    },
+    {
+      id: 328679,
+      lang: "ru",
+      date: "Wed Mar 29 2023 00:00:00 GMT+0300 (Moscow Standard Time)",
+      title:
+        "Мера длительного расщепления: что изменит появление ядерного оружия в Белоруссии",
+      description:
+        "Москва и Минск договорились разместить тактическое ядерное оружие на территории Белоруссии. Эти планы следует рассматривать в контексте политического торга вокруг Украины и эволюции белорусского президента Александра Лукашенко, который еще несколько лет назад подобные идеи отвергал, полагает специалист по постсоветскому пространству, автор Фонда Карнеги за международный мир Кирилл Кривошеев",
+      image: "",
+      source_id: 8,
+      category_id: 5,
+    },
+    {
+      id: 328670,
+      lang: "ru",
+      date: "Wed Mar 29 2023 00:00:00 GMT+0300 (Moscow Standard Time)",
+      title:
+        "Компьютер месяца, спецвыпуск. Собираем максимально дешевые игровые ПК, используя разные торговые площадки",
+      description:
+        "В рамках этого материала мы рассмотрели покупку комплектующих для сборки игровых системных блоков на разных торговых площадках: в крупных сетевых магазинах России, на AliExpress и «Авито». О том, где выгоднее покупать компьютерное железо, читайте далее \n#компьютермесяца #сборка #aliexpress #avito",
+      image:
+        "https://3dnews.ru/assets/external/illustrations/2023/03/25/1084029/0.jpg",
+      source_id: 6,
+      category_id: 1,
+    },
+    {
+      id: 328703,
+      lang: "ru",
+      date: "Tue Mar 28 2023 23:58:22 GMT+0300 (Moscow Standard Time)",
+      title:
+        "Минторг США ввел ограничения на экспорт против двух фирм из России и еще девяти структур",
+      description:
+        "Министерство торговли США ввело экспортные ограничения против 11 структур, среди которых 2 фирмы из России. Об этом сообщает ТАСС со ccылкой на сборник официальных документов американского правительства.\n\nСогласно его данным, бюро по ...",
+      image:
+        "https://img.gazeta.ru/files3/727/14682727/20211028_gaf_x99_202-pic905-895x505-52809.jpg",
+      source_id: 15,
+      category_id: 4,
+    },
+    {
+      id: 328705,
+      lang: "ru",
+      date: "Tue Mar 28 2023 23:55:40 GMT+0300 (Moscow Standard Time)",
+      title:
+        "Евростат: США вышли на первое место по поставкам нефти в Евросоюз к концу 2022 года",
+      description:
+        "США вышли на первое место по поставкам нефти в ЕС в конце 2022 года, Россия же сократила свою долю с 31% до 4%, сообщает Eurostat.\n\nПо его данным, в период с последнего квартала 2021 года до января 2022 года доля США составляла ...",
+      image:
+        "https://img.gazeta.ru/files3/441/16363441/shutterstock_1913735710-pic905-895x505-33183.jpg",
+      source_id: 15,
+      category_id: 4,
+    },
+  ],
+};
+
+const mainNews = data.items.slice(0, 3);
+const smallNews = data.items.slice(3, 12);
+
+const mainNewsTemplate = document.querySelector("#main-news-item");
+const smallNewsTemplate = document.querySelector("#small-news-item");
+const mainNewsContainer = document.querySelector(".articles__big-column");
+const smallNewsContainer = document.querySelector(".articles__small-column");
+
+mainNews.forEach((item) => {
+  const copyTemplate = mainNewsTemplate.content.cloneNode(true);
+  const category = data.categories.find(
+    (category) => category.id === item.category_id,
+  );
+  const source = data.sources.find((source) => source.id === item.source_id);
+
+  copyTemplate.querySelector(".main-article__title").innerText = item.title;
+  copyTemplate.querySelector(".main-article__text").innerText =
+    item.description;
+  copyTemplate.querySelector(".main-article__image").src = item.image;
+  copyTemplate.querySelector(".main-article__category").innerText =
+    category.name;
+  copyTemplate.querySelector(".main-article__source").innerText = source.name;
+
+  mainNewsContainer.appendChild(copyTemplate);
+});
+smallNews.forEach((item) => {
+  const copyTemplate = smallNewsTemplate.content.cloneNode(true);
+  smallNewsContainer.appendChild(copyTemplate);
+});
